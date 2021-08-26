1d24b5ed31f07fdd1eb7ae9738973be389d62df3
Matthew Gamble <git@matthewgamble.net>
2021-08-27 08:49:09 +1000

    Code style fix

--- tests/test_client.py
+++ tests/test_client.py
@@ -11,5 +11,7 @@ def test_check_status():
 def test_get_manifest_and_digest():
     url = mock_registry()
     manifest = Client(url).get_manifest(TEST_NAME, TEST_TAG)
-    assert manifest.digest == "sha256:6c3c624b58dbbcd3c0dd82b4c53f04194d1247c6eebdaab7c610cf7d66709b3b"
+    assert (
+        manifest.digest == "sha256:6c3c624b58dbbcd3c0dd82b4c53f04194d1247c6eebdaab7c610cf7d66709b3b"
+    )
     assert isinstance(manifest.content, dict)
